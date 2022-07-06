39f8d46ca07d8318a77e23cb68712330754d73e5
HeIsRazod <100656213+HeIsRazod@users.noreply.github.com>
2022-07-05 23:16:25 -0300

    Update Alumno.java

--- src/proyectotransversal/g7/Modelo/Alumno.java
+++ src/proyectotransversal/g7/Modelo/Alumno.java
@@ -86,7 +86,7 @@ public class Alumno {
 
     @Override
     public String toString() {
-        return "Alumno{" + "idAlumno=" + idAlumno + ", nombre=" + nombre + ", apellido=" + apellido + ", fechNac=" + fechNac + ", dni=" + dni + ", activo=" + activo + '}';
+        return apellido + " " + nombre + " " + dni;
     }
 
 
